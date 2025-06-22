190a34fdab21e1faa32068c08b2a1bf558501fd5
josesouza04 <josegabrielvds2004@outlook.com>
2025-06-22 00:35:26 -0300

    final cypress

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,4 +1,4 @@
-describe('testes de funcioanlidade', () => {
+describe('testes de funcionalidade', () => {
 
   beforeEach(() => {
     // Certifique-se de que esta é a URL correta do seu aplicativo
@@ -7,15 +7,8 @@ describe('testes de funcioanlidade', () => {
 
   it('deve calcular frete com sucesso (com interceptação)', () => {
     const cepValido = '12345678'; 
-    const freteEsperado = 15.50;
 
-    cy.intercept('GET', 'http://localhost:3000/shipping/' + cepValido, { // Adapte o endpoint da sua API de frete
-      statusCode: 200,
-      body: {
-        cep: cepValido,
-        value: freteEsperado
-      }
-    }).as('calculateShipping'); // Cria um alias para a requisição
+    cy.intercept('GET', 'http://localhost:3000/shipping/' + cepValido).as('calculateShipping'); // Cria um alias para a requisição
 
     cy.get('input[placeholder="Digite o CEP"]').first().click().type(cepValido); // .first() pega o primeiro se houver múltiplos
     cy.get('a[data-id="1"]').click();
@@ -24,7 +17,6 @@ describe('testes de funcioanlidade', () => {
 
     cy.get('.swal-icon--success__ring').should('be.visible');
     cy.get('.swal-title').should('contain', 'Frete');
-    cy.get('.swal-text').should('contain', 'O frete é:')
-                                  .and('contain', freteEsperado);
+    cy.get('.swal-text').should('contain', 'O frete é:');
   })
 })
